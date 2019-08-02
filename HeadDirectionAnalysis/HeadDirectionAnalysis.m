12beb5da45813461bfe3c5658af3b02f56d9c23f
kevinksit <kevinkamansit@gmail.com>
2019-08-02 15:44:24 -0700

    Continued development of the head direction analysis code

--- HeadDirectionAnalysis/HeadDirectionAnalysis.m
+++ HeadDirectionAnalysis/HeadDirectionAnalysis.m
@@ -1,8 +1,8 @@
-classdef HeadDirectionAnalysis < handle
+classdef HeadDirectionAnalysis < RawDataPlots & SummaryDataPlots
     
     % Make sure you have the circular statistics toolbox
     properties (Constant = true)
-        bin_edges double = [-180:10:180];
+        bin_width double = 10;
         radius    double = [120]; % 120mm
         thresh    double = 0.05; % p-value threshold for significant tuning
     end
@@ -28,43 +28,25 @@ classdef HeadDirectionAnalysis < handle
             
             obj.initializeData(data,floating);
         end
-%% Major analysis code
-        function findHeadDirectionCells(obj)
-            
-            binned_DFF = obj.binDFF(obj.workingData.heading,obj.workingData.DFF);
-            obj.analysisData.addData(binned_DFF);
-            
-            p = obj.detectCells(obj.workingData.DFF);
-            obj.analysisData.addData(p);
-            
-            isDirectionTuned = p < obj.thresh;
-            obj.analysisData.addData(isDirectionTuned);           
+
+        function findHeadDirectionCells(obj) % just a wrapper for full analysis later
         end
-        
-%% Visualization
-        function polarPlot(obj,c)
-            rho = obj.analysisData.binned_DFF(c,:);
-            rho = [rho rho(1)];
-            
-            theta = obj.bin_edges * pi/180;
-            pax = polaraxes();
-            polarplot(theta,rho);
-            set(pax,'ThetaZeroLocation','top')
-            set(pax,'ThetaDir','clockwise')
-        end
-        
 %% Setters and Getters       
         function setHeadingFlag(obj,val)
             obj.fixHeadingFlag = val;
             obj.initializeData(obj.initData,obj.initFloating);
         end
-        
     end
     
     
-    methods (Access = private)
+    methods (Access = public)
         function initializeData(obj,data,floating)
-            DFF = data.DFF;
+            try % temporarily to account of spikeInferenc/noSpikeInference
+                DFF = data.spikes;
+            catch
+                fprintf('Not spike inferred, using DFF\n')
+                DFF = data.DFF;
+            end
             X = floating.X;
             Y = floating.Y;
             alpha = floating.alpha;
@@ -84,12 +66,29 @@ classdef HeadDirectionAnalysis < handle
         
         function out = detectCells(obj,data)
             for ii = 1:size(data,1)
-                out(ii) = circ_otest(data(ii,:));
+                otest(ii) = circ_otest(data(ii,:)); %nonuniformity
+                if otest(ii) < obj.thresh
+                    rtest(ii) = circ_rtest(data(ii,:)); %unimodality
+                else 
+                    rtest(ii) = 1;
+                end
             end
+            out = (otest<obj.thresh) & (rtest<obj.thresh);
         end
         
+        function out = getPreferredDirection(obj,data)
+            fprintf('Getting preferred directions \n');
+           for ii = 1:size(data,1)
+              % lm = fit([1:length(data(ii,:))]',data(ii,:)','gauss1','Upper',[Inf 36 Inf], 'Lower', [-Inf 0 -Inf]);
+             %  out(ii) = lm.b1;
+             [~,out(ii)] = max(data(ii,:)); 
+           end 
+           
+           out=out*obj.bin_width - obj.bin_width; % convert to degrees, accounting for the extra 10 you get because -180:10:180
+        end
         function out = binDFF(obj,heading,data)
-            groups = discretize(heading,obj.bin_edges);
+            bin_edges = -180:obj.bin_width:180; % Because alpha is [-180,180];
+            groups = discretize(heading,bin_edges);
             u_groups = unique(groups);
             for c = 1:size(data,1)
                 for bin = 1:length(u_groups)
@@ -106,4 +105,26 @@ classdef HeadDirectionAnalysis < handle
     end
     
 end
+
+        
+        %% Major analysis code 
+        
+        % when you're done messing around in the script, you can incorporate as a big thing, but until then, run the individual functions in the script ok
+        
+        %{
+        function findHeadDirectionCells(obj)
+            
+            binned_DFF = obj.binDFF(obj.workingData.get('heading'),obj.workingData.get('DFF'));
+            obj.analysisData.add(binned_DFF);
+            
+            p = obj.detectCells(obj.workingData.get('DFF'));
+            pref_dir = obj.getPreferredDirection(obj.analysisData.get('binned_DFF'));
+            
+            obj.analysisData.add('p','pref_dir');
+            
+            isDirectionTuned = p < obj.thresh;
+            obj.analysisData.add(isDirectionTuned);
+
+        end
+        %}
 
--- HeadDirectionAnalysis/Neurotar_headDirectionAnalysis_master.m
+++ HeadDirectionAnalysis/Neurotar_headDirectionAnalysis_master.m
@@ -1,20 +1,52 @@
 %Neurotar_headDirectionAnalysis
 
+% Sample data
+%{
 load('D:\Code\NeurotarCode\SampleData\floating_data_WTR010_07_24_session1.mat')
 load('D:\Code\NeurotarCode\SampleData\TSeries-07242019-1414-001_registered_data.mat')
+%}
+
+% if you need it...
+n = NeurotarDataExtractor();
+n.saveData;
+
 
 hdp = HeadDirectionPreprocessor(data,floating);
 %hdp.setForceTimeLock(true);
 %hdp.processData;
 
-hda = HeadDirectionAnalysis(hdp.workingData.data,hdp.workingData.floating);
+hda = HeadDirectionAnalysis(hdp.workingData.get('data'), hdp.workingData.get('floating'));
 
 % If you want use raw alpha, but not recommended...
 %hda.setHeadingFlag(false);
 
-hda.findHeadDirectionCells();
 
-% visualize?
-hda.polarPlot(1)
+%% Analysis 
+binned_DFF = hda.binDFF(hda.workingData.get('heading'),hda.workingData.get('DFF')); % bin DFF
+DFF = hda.workingData.get('DFF');
+isDirectionTuned = hda.detectCells(DFF); % Detect non-circularly uniform cells
+pref_dir = hda.getPreferredDirection(binned_DFF); % Get preferred directions
+
+hda.analysisData.add('binned_DFF','p','pref_dir','isDirectionTuned');
+
+
+
+% Current issue: not sure the best way to be able to figure out if they're actually "head directiony"
+
+% what's the best way to determine that? Idk we'll work on this...
+
+% Visualizing all the cells first...
+hda.setData(binned_DFF);
+
+
+hda.analysisData.exportVar('isDirectionTuned','pref_dir');
+cells2plot = find(isDirectionTuned);
+for ii = 1:length(cells2plot)
+    c = cells2plot(ii);
+hda.polarPlot(c,'LineWidth',5);
+title(sprintf('Cell #%d, prefdir: %0.1f',ii,pref_dir(c)))
+pause
+end
+
 
 
