d169a8c5153241eafcd4600e5db2c1207ff27888
Kevin Sit <kevinkamansit@gmail.com>
2019-08-12 09:12:31 -0700

    Many minor changes here and there

--- HeadDirectionAnalysis/Neurotar_headDirectionAnalysis_master.m
+++ HeadDirectionAnalysis/Neurotar_headDirectionAnalysis_master.m
@@ -7,21 +7,23 @@ load('D:\Code\NeurotarCode\SampleData\TSeries-07242019-1414-001_registered_data.
 %}
 
 % if you need it...
-n = NeurotarDataExtractor();
-n.saveData;
+%n = NeurotarDataExtractor();
+
+%n.saveData;
 
 
 hdp = HeadDirectionPreprocessor(data,floating);
 %hdp.setForceTimeLock(true);
 %hdp.processData;
-
 hda = HeadDirectionAnalysis(hdp.workingData.get('data'), hdp.workingData.get('floating'));
+
+
 
 % If you want use raw alpha, but not recommended...
 hda.setHeadingFlag(false);
 
 
-%% Analysis 
+%% Preprocessing 
 binned_DFF = hda.binDFF(hda.workingData.get('heading'),hda.workingData.get('DFF')); % bin DFF
 DFF = hda.workingData.get('DFF');
 % Not currently running below because I don't have the toolbox
@@ -32,20 +34,22 @@ hda.analysisData.add('binned_DFF','pref_dir');
 
 
 
+%% Working on better analysis
 % Current issue: not sure the best way to be able to figure out if they're actually "head directiony"
 [headDirIdx] = hda.calculateHeadDirectionIdx();
 
-isHeadDir = headDirIdx > 0.2;
-
+isHeadDirection = mean(headDirIdx,2) > 0.2;
 
 % what's the best way to determine that? Idk we'll work on this...
 
+
+%% Visualization
 % Visualizing all the cells first...
 hda.setData(binned_DFF);
 
 
 hda.analysisData.exportVar('isDirectionTuned','pref_dir');
-cells2plot = find(isHeadDir);
+cells2plot = find(isHeadDirection);
 for ii = 1:length(cells2plot)
     c = cells2plot(ii);
 hda.polarPlot(c,'LineWidth',5);
