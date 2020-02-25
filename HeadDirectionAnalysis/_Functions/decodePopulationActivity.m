702027e8425aceaa32aed55d6083d7a65663b9f0
kevinksit <kevinkamansit@gmail.com>
2020-02-25 13:27:57 -0800

    Updated the decoder to have variable contribution

--- HeadDirectionAnalysis/_Functions/decodePopulationActivity.m
+++ HeadDirectionAnalysis/_Functions/decodePopulationActivity.m
@@ -1,128 +1,130 @@
-function [predicted_heading, heading_distribution] = decodePopulationActivity(tuning_curve, time_series, display_flag, normalize_flag)
-% At each time point (t), each given neuron contributes a set of vectors, defined by its tuning curve. This set
-% of vectors is then scaled by the activity of that neuron at a given time point, and averaged across all neurons
-% to get a single population activity vector
-if nargin < 3 || isempty(display_flag)
-    display_flag = true;
-end
+function [predicted_heading, heading_distribution] = decodePopulationActivity(tuning_curve, time_series, is_head_direction)
 
-if nargin < 4 || isempty(normalize_flag)
-    normalize_flag = false;
+if nargin < 3 || isempty(is_head_direction)
+	is_head_direction = true(1, size(tuning_curve, 1));
 end
+%% Rescale all rows, sets each row to be [0, 1]
 
-% Rescale all rows
+%% Updated 25Feb2020 KS
+% Changelog: Changed singleton removal to use bwmorph to reduce the chances of getting caps, was getting maxmin'd values a lot
+%			 Changed construction of the peak finder for better clarity and performance
+%			 Added variable contribution, based on the cell type
+
+%% Parameters
+memory = 5; % How many samples to look back for fitting a line for prediction, generally improves performance
+pre_post_samples = 10; % how many samples to mean over when creating the heading distribution
+% Increasing this value definitely improves performance
+
 tuning_wts = rescale(tuning_curve,...
     'InputMin', min(tuning_curve, [], 2),...
     'InputMax', max(tuning_curve, [], 2));
 
-% Will's thing..
-%tuning_wts(tuning_wts < 0.75) = 0;
 
-if normalize_flag
-    % cap it
-    cap = prctile(time_series', 99);
-    for ii = 1:size(time_series, 1)
-        temp = time_series(ii, :);
-        temp(temp > cap(ii)) = cap(ii);
-        time_series(ii, :) = temp;
-    end
-    
-    time_series = rescale(time_series,...
-        'InputMin', min(time_series, [], 2),...
-        'InputMax', max(time_series, [], 2));
-end
-
-%             for t = 1:size(timeseries, 2)
-%                 temp = (max(timeseries(:, t), 0) .* smoothed_wts) ./ sum(smoothed_wts); % nonzero weights are lost
-%                 direction_distributions(t, :) = sum(temp);
-%             end
-
-% this is janky af but ok, not sure if i want to keep this
-sigmoid_sharpness = 5; % > 5 or else you lose the asymptotes
-modelfun = @(x)   (tanh(sigmoid_sharpness * (x - 0.5)) + 1) .* 0.5; %10 defines sharpness
-
-group_method = 'movmean';
-switch group_method
-    case 'bin'
-        bin_size = 10;
-        ct=1;
-        for t = 1:bin_size:size(time_series, 2)
-            temp = (max(mean(time_series(:, t:t + 9), 2), 0) .* tuning_wts) ./ sum(tuning_wts); % nonzero weights are lost
-            heading_distribution(ct, :) = sum(temp);
-            ct=ct+1;
-        end
-        
-    case 'movmean'
-        pre_post_samples = 5;
-        heading_distribution = zeros(size(time_series, 2), size(tuning_wts, 2));
-        for t = 1:size(time_series, 2)
-            %temp = (max(mean(timeseries(:, max(t-pre_post_samples, 1) : min(t+pre_post_samples, length(timeseries))), 2), 0) .* tuning_wts) ./ sum(tuning_wts); % nonzero weights are lost
-            activity = max(mean(...
-                time_series(:, max(t-pre_post_samples, 1) : min(t+pre_post_samples,...
-                length(time_series))), 2), 0);
-            heading_distribution(t, :) = sum((activity) .* tuning_wts) ./ sum(tuning_wts); % nonzero weights are lost);
-        end
-end
+tuning_wts(~is_head_direction, :) = tuning_wts(~is_head_direction, :) * 0.3; % reduce the contribution of non-hd cells
 
 %{
-bound = @(x, bl, bu) min(max(x, bl), bu);
-% modelfun = @(b, x) b(1) + ...
-%     max([0, b(2)]) * exp(-(x(:, 1) - bound(b(3), 0, x(end, 1))) .^ 2 / (2 * b(4) .^ 2)) + ...
-%     max([0, b(5)]) * exp(-(x(:, 1) - bound(b(6), 0, x(end, 1))) .^ 2 / (2 * b(4) .^ 2)); % Based on Michael natneuro paper
-
-modelfun = @(b, x) b(1) + ...
-    max([0, b(2)]) * exp(-(x(:, 1) - bound(b(3), 0, x(end, 1))) .^ 2 / (2 * b(4) .^ 2));
-
-%% adding some "memory"
-
-memory = 0;
-for ii = 1:size(heading_distribution, 1)
-    % ok for now, but to be more clever, we should have weighting dependent on the distance to the previous memory, scaling
-    % fctors that decrease with increasing distance...
-    current_heading_distribution = mean(heading_distribution(max(1, ii - memory) : ii, :), 1);
-
-    coeffs(ii, :) = fitDoubleGaussian(current_heading_distribution, modelfun);
+% cap it
+cap = prctile(time_series', 95); % removing mega spikes
+for ii = 1:size(time_series, 1)
+    temp = time_series(ii, :);
+    temp(temp > cap(ii)) = cap(ii);
+    time_series(ii, :) = temp;
 end
 
-predicted_heading = coeffs(:, 3); % Location of first (bigger) peak
+
+time_series = rescale(time_series,...
+    'InputMin', min(time_series, [], 2),...
+    'InputMax', max(time_series, [], 2));
 %}
 
 
-[~, predicted_heading] = max(heading_distribution, [], 2);
 
-%% Subfunctions
-function [coefficients, fit_quality] = fitDoubleGaussian(data, model)
-% The plan is to use a mixture of gaussian models to get a better idea of the bilobality stuff
-% Prepare options for linear fitting
-opts = statset('nlinfit');
-opts.FunValCheck = 'on';
-%opts.Display = 'final';
+%% Get the heading distribution, a matrix of possible headings at each given time point
+heading_distribution = zeros(size(time_series, 2), size(tuning_wts, 2));
 
-beta0 = prepareBeta0(data);
-tbl = table([1:size(data, 2)]', data');
-try
-    mdl = fitnlm(tbl, model, beta0, 'Options', opts);
-    coefficients = mdl.Coefficients{:, 'Estimate'};
-    fit_quality = mdl.Rsquared.Adjusted;
-catch
-    coefficients = NaN;
-    fit_quality = 0;
-    
-    
+for t = 1:size(time_series, 2)
+    %temp = (max(mean(timeseries(:, max(t-pre_post_samples, 1) : min(t+pre_post_samples, length(timeseries))), 2), 0) .* tuning_wts) ./ sum(tuning_wts); % nonzero weights are lost
+    activity = max(mean(...
+        time_series(:, max(t-pre_post_samples, 1) : min(t+pre_post_samples,...
+        length(time_series))), 2), 0);
+    heading_distribution(t, :) = sum((activity) .* tuning_wts) ./ sum(tuning_wts); % nonzero weights are lost);
 end
 
 
-function beta0 = prepareBeta0(data)
-[sorted_values, sorted_idx] = sort(data, 'descend');
-mag1 = max([sorted_values(1), 1]);
-loc1 = sorted_idx(1);
-std1 = 2;
+for ii = 1:size(heading_distribution, 1)
+    current_heading_distribution = mean(heading_distribution(max(1, ii) : ii, :), 1); % what's the purpose of this line?
+    current_heading_distribution = heading_distribution(ii, :); % Does this cover it?
+    threshold = mean(current_heading_distribution) + 1 * std(current_heading_distribution);
+    
+    % break down current vector into logical vector
+    curr_peak = current_heading_distribution > threshold;
+    
+    % eliminate singletons
+    % We want to give some leeway to the singletons if they're on the end, but not if they're only on the end, right?
 
-% find the second peak
-wrapN = @(x, N) (1 + mod(x-1, N));
-mag2 = mag1 / 2; % Random guess
-loc2 = wrapN(loc1 + length(data) / 2, length(data));
+    curr_peak = bwmorph(curr_peak, 'clean'); % this should remove all singletons, but preserve ones on the end, if they have neighbors
+    % will have issues if you only have 1 true value on the side, leading to a trail on the other side but probably okay...
+   %{
+ for t = 1:size(curr_peak, 2)
+        if t == 1
+        elseif t == size(curr_peak, 2)
+            %t == 2:((length(t))-1)
+        else
+            if curr_peak(t - 1) == 0 && curr_peak(t + 1) == 0
+                curr_peak(t) = 0;
+            end
+        end
+    end
+%}
+    
+    % number of changes in vector/2 to discount negative changes
+    change = diff(curr_peak(:));
+    num_peak = ceil((nnz(unique(cumsum([true;diff(change)~=0]).*(change~=0))))/2); % wtf is going on here... where did you get this
+    %% Cell array method of peak grouping + meaning
 
-beta0 = [0, mag1, loc1, std1, mag2, loc2];
+    % find and split peaks
+    peak_point = find(curr_peak == 1);
+    split = find([0 (diff(peak_point))>1]);
+    
+    
+    % group peaks into cell array, i want to refactor this later KS 25Feb2020
+    if num_peak >= 2
+	    for jj = 1:num_peak % A better representation of what we're doing
+	    	if num_peak >= 2
+	    		if jj == 1
+	    			peak_group{jj} = peak_point(1:split(jj) - 1);
+	    		elseif jj == length(split)+1
+	    			peak_group{jj} = peak_point(split(jj - 1):end);
+	    		else
+	    			peak_group{jj} = peak_point(split(jj - 1) : split(jj) - 1);
+	    		end
 
-
+	            % avg_cells = cell(1, size(curr_peak, 2));
+	            % find mean point of each peak
+	            % avg_cells{t} = cellfun(@(x) mean(x(:)), peak_group, 'UniformOutput', false);
+	            avg_cells = cellfun(@(x) mean(x(:)), peak_group, 'UniformOutput', false);
+	            avg_array = vertcat(avg_cells); % Some of these might not be necessary...
+	            avg_peak = cell2mat(avg_array);
+	        end
+	    end
+	    clear peak_group % to ensure that the next run is not affected by previous run
+	else
+		avg_peak = mean(peak_point);
+	end
+    
+    if ii <= memory
+        predicted_heading(ii) = min(avg_peak); % why min?
+    else        
+        % correlate previous points
+        peak_fit = polyfit([(ii-memory):(ii-1)], predicted_heading((ii-memory):(ii-1)), 1);
+        if peak_fit(:, 1) <= 0.1 && peak_fit(:, 1) >= -0.1 % If low slope?, ie if slope is [-0.1, 0.1] 
+            prev_output = ceil(predicted_heading(ii-1)); % take the highest? why? why not lower?
+        else
+            prev_output = polyval(peak_fit, ii);
+        end
+        
+        % find current peak closest to the previous peak
+        [~, location] = min(abs(avg_peak - prev_output));
+        predicted_heading(ii) = avg_peak(location);
+    end
+end
